1af52ba6f480f6ddf1b695118c50eb93021ace34
Sebastian Aguirre <sebastian.aguirre@wiga.io>
2022-05-21 18:16:07 -0500

    se agrego cadena de connection para azure

--- FacontaryFunctions/Common/ConectMYSQL.cs
+++ FacontaryFunctions/Common/ConectMYSQL.cs
@@ -18,7 +18,8 @@ namespace FacontaryFunctions.Common
 
                 if (string.IsNullOrEmpty(conStr)) // Azure Functions App Service naming convention
                 {
-                    conStr = System.Environment.GetEnvironmentVariable($"MYSQLCONNSTR_MySqlConnection");
+                    //conStr = System.Environment.GetEnvironmentVariable($"MYSQLAZURECONNSTR_MySqlConnection", EnvironmentVariableTarget.Process);
+                    conStr = "Server=facontary.mysql.database.azure.com; Port=3306; Database=facontary; Uid=zebax; Pwd=Ernalituyd_#45%?2suq3.xsa; SslMode=Preferred;";
                 }
                 MySqlConnection connection = new MySqlConnection(conStr);
                 await connection.OpenAsync();
